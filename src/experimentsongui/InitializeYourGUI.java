56f58a08f66b3202899ec78eedc7a47c6b08bff0
Gianluca Consoli - gianluca.consoli@studio.unibo.it <gianluca.consoli@studio.unibo.it>
2021-11-19 16:53:15 +0100

    Created a new class InitializeYourGUI.java

---
+++ src/experimentsongui/InitializeYourGUI.java
@@ -0,0 +1,18 @@
+package experimentsongui;
+
+public final class InitializeYourGUI {
+
+    private InitializeYourGUI() {
+    }
+    /**
+     *  @param s
+     *          various args...
+     */
+    public static void main(final String... s) {
+
+        final FirstGUI g1 = new FirstGUI();
+        g1.display();
+
+    }
+
+}
