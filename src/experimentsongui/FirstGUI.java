5d421d7ae4554f2e57785b50b592280bc9e3e0db
Gianluca Consoli - gianluca.consoli@studio.unibo.it <gianluca.consoli@studio.unibo.it>
2021-11-19 16:53:55 +0100

    Modified FirstGUI.java

--- src/experimentsongui/FirstGUI.java
+++ src/experimentsongui/FirstGUI.java
@@ -1,4 +1,4 @@
-package experimentsGUI;
+package experimentsongui;
 
 import javax.swing.JButton;
 import javax.swing.JFrame;
@@ -10,27 +10,34 @@ import java.awt.Dimension;
  *  My Expertiments on GUI
  * 
  */
-public class FirstGUI {
+public class FirstGUI implements GUIInterface {
 
     private static final String TITLE = "Titolo del Frame";
     private static final int WIDTH = 500;
     private static final int HEIGHT = 500;
+    private final JFrame frame = new JFrame();
 
-    public static void main(final String... args) {
+    public FirstGUI() {
 
         // Parte frame
-        final JFrame frame = new JFrame();
         frame.setTitle(FirstGUI.TITLE);
         frame.setSize(new Dimension(WIDTH, HEIGHT));
         frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
-
-        frame.setVisible(true);
 
         // Parte panel con bottoni
         final JPanel p1 = new JPanel();
         frame.setContentPane(p1);
 
         p1.add(new JButton("Prova Bottone"));
+
+    }
+
+    /**
+     *  {@inheritDoc}
+     */
+    public void display() {
+
+        frame.setVisible(true);
 
     }
 
