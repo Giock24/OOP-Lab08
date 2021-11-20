b80b9ee4750f34b48d444d16f379c97ebda2dccb
Gianluca Consoli <gianluca.consoli@studio.unibo.it>
2021-11-20 15:55:51 +0100

    Created MyFrame.java

---
+++ src/experimentsongui/MyFrame.java
@@ -0,0 +1,18 @@
+package experimentsongui;
+
+import javax.swing.JFrame;
+import javax.swing.JPanel;
+
+public class MyFrame extends JFrame {
+
+    /**
+     * 
+     */
+    private static final long serialVersionUID = 1L;
+    private JPanel jp;
+
+    public MyFrame(final String namePanel, final JPanel jp) {
+
+    }
+
+}
