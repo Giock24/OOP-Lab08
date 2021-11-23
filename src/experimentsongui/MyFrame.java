bb40a688e4c5f6425298d6228b50ed77e73084f5
Gianluca Consoli <gianluca.consoli@studio.unibo.it>
2021-11-23 12:23:21 +0100

    Created MyFrame to initalize JFrame, JPanel and Layout

--- src/experimentsongui/MyFrame.java
+++ src/experimentsongui/MyFrame.java
@@ -1,18 +1,43 @@
 package experimentsongui;
 
+import java.awt.LayoutManager;
+
 import javax.swing.JFrame;
 import javax.swing.JPanel;
 
+/**
+ * 
+ * This class set a personal JFrame, with JPanel and setting Layout. 
+ */
 public class MyFrame extends JFrame {
 
     /**
      * 
      */
     private static final long serialVersionUID = 1L;
-    private JPanel jp;
+    private final JPanel jp;
 
-    public MyFrame(final String namePanel, final JPanel jp) {
+    /**
+     * 
+     * @param nameFrame
+     *      JFrame's TITLE
+     * @param lay
+     *      Layout to set on new JPanel
+     */
+    public MyFrame(final String nameFrame, final LayoutManager lay) {
+        super(nameFrame);
+        this.jp = new JPanel(lay);
 
+        this.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+        this.setContentPane(this.jp);
+    }
+
+    /**
+     * 
+     * @return new JPanel
+     */
+    public JPanel getMainPane() {
+        return this.jp;
     }
 
 }
